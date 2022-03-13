dc3ea50b6216eab0927cb1436685deb25ab50a0d
Jean-Emrick <gestionjerioux@gmail.com>
2022-03-12 19:54:50 -0500

    -

--- src/components/SomeTracker/Intake.js
+++ src/components/SomeTracker/Intake.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
 
 const Intake = (props) => {
-  
   return <div className="mb-5">ALLO</div>;
-
+};
 export default Intake;
