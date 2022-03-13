d12ebb4ead028489f9e5dc465aeb98cc304636eb
Jean-Emrick <gestionjerioux@gmail.com>
2022-03-13 01:06:10 -0500

    :)

--- src/components/App.js
+++ src/components/App.js
@@ -325,16 +325,6 @@ class App extends React.Component {
           >
             <img src={PoweredByAkash} alt="Powered by Akash" width={200} />
           </a>
-<<<<<<< HEAD
-          <p className="col-md-4 d-flex align-items-center justify-content-center me-lg-auto">
-            <span className="d-none d-sm-inline me-1">Built with 🧡&nbsp; by</span> <a  href="https://stakefrites.co/" class="link-dark text-decoration-none" target="_blank" rel="noreferrer">Stake Frites (🥩,🍟)&nbsp;</a>and&nbsp;<a class="link-dark text-decoration-none" href="https://ecostake.com" target="_blank" rel="noreferrer"> ECO Stake 🌱</a>
-          </p>
-          <div className="col-md-3" align="right">
-            <a href="https://twitter.com/stakefrites_" alt="Twitter" target="_blank" rel="noreferrer" className="link-dark text-decoration-none">
-              <Twitter color="black" size={24} />
-            </a>
-            <a href="https://github.com/stakefrites" alt="Github" target="_blank" rel="noreferrer" style={{marginLeft:'10px'}} className="link-dark text-decoration-none">
-=======
           <a
             href="https://stakefrites.co/"
             target="_blank"
@@ -361,7 +351,6 @@ class App extends React.Component {
               rel="noreferrer"
               className="link-dark text-decoration-none"
             >
->>>>>>> 26d25a5e6d06241ae717efa9629854b4a9bd0ed5
               <Github color="black" size={24} />
             </a>
           </div>
